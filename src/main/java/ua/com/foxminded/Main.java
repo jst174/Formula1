1ec90935d85ef9c01be928bac94725fa8071433e
Eugene Stepanenko <jst1749.7@gmail.com>
2021-06-10 07:46:55 +0500

    change catch in main

--- src/main/java/ua/com/foxminded/Main.java
+++ src/main/java/ua/com/foxminded/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         ClassLoader classLoader = Main.class.getClassLoader();
         try {
-            File abbreviations = new File(classLoader.getResource("abbreviations.txt").getFile());
+            File abbreviations = new File(classLoader.getResource("abbreviqations.txt").getFile());
             File start = new File(classLoader.getResource("start.log").getFile());
             File end = new File(classLoader.getResource("end.log").getFile());
             RacersRepository racers = new RacersRepository(abbreviations, start, end);
             RacersListFormater formater = new RacersListFormater();
             System.out.println(formater.format(racers.getRacers(), 15));
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
